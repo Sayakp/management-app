bdaaead7ef8e5d3243e311c5bfd3304f7b2bb1b5
Daniel <skap.tk@gmail.com>
2024-09-02 22:14:49 -0500

    Initial commit

---
+++ management-spring-app/src/main/java/org/daniel/managementspringapp/ManagementSpringAppApplication.java
@@ -0,0 +1,13 @@
+package org.daniel.managementspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementSpringAppApplication.class, args);
+    }
+
+}
---
+++ management-spring-app/src/test/java/org/daniel/managementspringapp/ManagementSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniel.managementspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
